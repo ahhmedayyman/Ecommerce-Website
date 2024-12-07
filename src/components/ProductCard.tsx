cbf71327f9ab03e44cfb99af052a68da04d9ca7f
Ahmed Ayman <ahhmedayyman99@gmail.com>
2024-12-07 23:04:10 +0200

    image component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@
 // import { cn } from "@/lib/utils";
 
 import Button from "./ui/Button";
+import Image from "./ui/image";
 
 interface IProps {
   title: string;
@@ -19,11 +20,7 @@ const ProductCard = ({
   return (
     <div className="border rounded-xl p-2 flex flex-col w-full md:w-mdwidth lg:w-lgwidth bg-white shadow-sm hover:shadow-md transition-all duration-300">
       <div className="w-full h-[350px] overflow-hidden rounded-lg mb-4 group">
-        <img
-          className="w-full h-full object-cover object-left-top transform group-hover:scale-105 transition-transform duration-500"
-          src={imageUrl}
-          alt={title}
-        />
+        <Image imageURL={imageUrl} alt={title} className="w-full h-full object-cover object-left-top transform group-hover:scale-105 transition-transform duration-500" />
       </div>
 
       <div className="flex-1 flex flex-col">
@@ -31,17 +28,18 @@ const ProductCard = ({
           {title}
         </h3>
 
-        <p className="text-sm text-gray-500 mb-2">{description}</p>
+        <p className="text-sm text-gray-500 mb-4">{description}</p>
         <div className="flex flex-row items-center space-x-2 mb-2">
           <span className="w-5 h-5 bg-indigo-500 rounded-full border cursor-pointer" />
           <span className="w-5 h-5 bg-yellow-500 rounded-full cursor-pointer" />
           <span className="w-5 h-5 bg-red-500 rounded-full cursor-pointer" />
         </div>
 
-        <div className="mb-2">
-          <span className="text-lg text-indigo-500">
+        <div className="mb-4 flex justify-between items-center">
+          <span className="text-lg font-semibold text-indigo-500">
             ${price.toFixed(2)}
           </span>
+          <Image imageURL={imageUrl} alt={title} className="w-10 h-10 rounded-full object-cover object-left-top" />
         </div>
 
         <div className="flex justify-between gap-2">
