bef150ac24f25a2ce2e9de12c3f6a3a024ad22a9
Ahmed Ayman <ahhmedayyman99@gmail.com>
2024-12-14 17:35:00 +0200

    style footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import ProductCard from "./components/ProductCard";
 import Navbar from "./components/Navbar";
-import {productList} from "./data";
+import { productList } from "./data";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const renderProductList= productList.map(product => <ProductCard key={product.id} product={product}/>)
+  const renderProductList = productList.map((product) => (
+    <ProductCard key={product.id} product={product} />
+  ));
   return (
     <>
       <div>
         <Navbar />
       </div>
       <div className="container mx-auto">
-       <div className="flex justify-between flex-wrap gap-3 w-custom mt-4">
-        {renderProductList}
-       </div>
-       
+        <div className="flex justify-between flex-wrap gap-3 w-custom mt-4">
+          {renderProductList}
+        </div>
+      </div>
+      <div>
+        <Footer />
       </div>
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
-interface IProps {
 
-}
-
-const Footer = ({}: IProps) => {
+const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto flex justify-center items-center">
+        <p className="text-sm font-normal">Developed By Ahmed Ayman</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
